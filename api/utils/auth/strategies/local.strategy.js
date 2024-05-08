97434075d8631008c0467ec93568939ff851419a
Jose Luis <wildchamo@gmail.com>
2024-05-08 18:15:28 -0500

    passport is cool

--- api/utils/auth/strategies/local.strategy.js
+++ api/utils/auth/strategies/local.strategy.js
@@ -5,20 +5,25 @@ const boom = require("@hapi/boom");
 const { verifyHash } = require("../../encrypt");
 
 const service = new UserService();
-const LocalStrategy = new Strategy(async (email, password, done) => {
-  try {
-    const user = await service.findByEmail(email);
-    if (!user) {
-      done(boom.unauthorized("User not found"), false); 
+const LocalStrategy = new Strategy(
+  {
+    usernameField: "email",
+  },
+  async (email, password, done) => {
+    try {
+      const user = await service.findByEmail(email);
+      if (!user) {
+        done(boom.unauthorized("User not found"), false);
+      }
+      const isMatch = await verifyHash(password, user.password);
+      if (!isMatch) {
+        done(boom.unauthorized("Password doesn't match"), false);
+      }
+      done(null, user);
+    } catch (error) {
+      done(error, false);
     }
-    const isMatch = await verifyHash(password, user.password);
-    if (!isMatch) {
-      done(boom.unauthorized("Password doesn't match"), false);
-    }
-    done(null, user);
-  } catch (error) {
-    done(error, false);
   }
-});
+);
 
 module.exports = LocalStrategy;
